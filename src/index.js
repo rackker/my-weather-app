f27bafe51af1245dfeff91e52e9284197f395823
rackker <80078166+rackker@users.noreply.github.com>
2022-08-19 17:20:02 +1000

    dynamic background & cleanup
    optimised code, made APIkey universal variable.
    Added dynamically changing background gradients based on time of day in selected city.
    Removed unit conversion elements.
    Cleaned up some CSS. Adjusted colours to suit day part backgrounds.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,64 @@
+function backgroundColourChange(localTime) {
+  let bodyCSS = document.querySelector("#body-div");
+  let cityName = document.querySelector("#city-name");
+  let weatherNow = document.querySelector("#main-temp");
+  let link = document.querySelector("a");
+  //sunrise colours
+  if (5 <= localTime && 8 >= localTime) {
+    bodyCSS.removeAttribute("class");
+    bodyCSS.classList.add("sunrise-background");
+    cityName.style.color = "rgb(0 153 52)";
+    weatherNow.style.color = "rgb(0 153 52)";
+    bodyCSS.style.color = "rgb(193 0 0)";
+  }
+  //sunset colours
+  else if (18 <= localTime && 21 >= localTime) {
+    bodyCSS.removeAttribute("class");
+    bodyCSS.classList.add("sunset-background");
+    cityName.style.color = "rgba(250, 171, 49, 1)";
+    weatherNow.style.color = "rgba(250, 171, 49, 1)";
+    bodyCSS.style.color = "white";
+  }
+  //nighttime colours
+  else if (21 < localTime || 5 > localTime) {
+    bodyCSS.removeAttribute("class");
+    bodyCSS.classList.add("nighttime-background");
+    cityName.style.color = "#caafff";
+    weatherNow.style.color = "#caafff";
+    bodyCSS.style.color = "white";
+    link.style.color = "white";
+  }
+  //daytime colours
+  else {
+    bodyCSS.removeAttribute("class");
+    bodyCSS.classList.add("daytime-background");
+    cityName.style.color = "#d7eef4";
+    weatherNow.style.color = "#d7eef4";
+    bodyCSS.style.color = "rgb(7,62,76)";
+    link.style.color = "black";
+  }
+}
+
+//converting timezones
+function changeTimeZone(response) {
+  let cityTimeZone = response.data.timezoneId;
+  let updatedTime = new Date().toLocaleString("en-AU", {
+    timeZone: cityTimeZone,
+    weekday: "long",
+    hour: "numeric",
+    minute: "2-digit",
+  });
+  localDayTime.innerHTML = updatedTime;
+
+  let newTime = new Date(response.data.time);
+  backgroundColourChange(newTime.getHours());
+}
+
+let apiKey = "721dfdcfc09e07da4b6904753634db8b";
 window.onload = melbRadio();
 
 //forecast API function
 function getForecast(coordinates) {
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let lat = coordinates.lat;
   let lon = coordinates.lon;
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
@@ -49,50 +105,42 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-    <div class="row justify-content-md-center">
-    <div class="col col-lg-2 text-end fw-bold">${maxTemp}°C</div>
-    <div class="col-md-auto text-center">
-    <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
-    alt="${day.weather[0].description}"
-    id="forecast-icon"/>
-    </div>
-    <div class="col col-lg-2">${formatDay(day.dt)}</div>
-    </div>
-    `;
+      <div class="row justify-content-md-center">
+      <div class="col col-lg-2 text-end fw-bold">${maxTemp}°C</div>
+      <div class="col-md-auto text-center">
+      <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+      alt="${day.weather[0].description}"
+      id="forecast-icon"/>
+      </div>
+      <div class="col col-lg-2">${formatDay(day.dt)}</div>
+      </div>
+      `;
     }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 //format day of week in forecast
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   return days[day];
 }
 
 //update time under today's weather with time relevant to the displayed city
 function lastUpdateTime(timestamp) {
   let date = new Date(timestamp);
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   let day = days[date.getDay()];
   let hours = [
     12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -118,21 +166,8 @@ function lastUpdateTime(timestamp) {
   return `Last updated ${day} ${hour}:${mins} ${amPM}`;
 }
 
-//converting timezones
-function changeTimeZone(response) {
-  let cityTimeZone = response.data.timezoneId;
-  let updatedTime = new Date().toLocaleString("en-AU", {
-    timeZone: cityTimeZone,
-    weekday: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  localDayTime.innerHTML = updatedTime;
-}
-
 //radio buttons mapped to Melbourne, Sydney & Brisbane weather API call
 function melbRadio() {
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let melbLat = -37.81;
   let melbLon = 144.96;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${melbLat}&lon=${melbLon}&appid=${apiKey}&units=metric`;
@@ -145,7 +180,6 @@ let melbRadioChecked = document.querySelector("#radioCitySelector1");
 melbRadioChecked.addEventListener("click", melbRadio);
 
 function sydRadio() {
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let sydLat = -33.868;
   let sydLon = 151.2;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${sydLat}&lon=${sydLon}&appid=${apiKey}&units=metric`;
@@ -158,7 +192,6 @@ let sydRadioChecked = document.querySelector("#radioCitySelector2");
 sydRadioChecked.addEventListener("click", sydRadio);
 
 function brisRadio() {
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let brisLat = -27.469;
   let brisLon = 153.02;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${brisLat}&lon=${brisLon}&appid=${apiKey}&units=metric`;
@@ -172,7 +205,6 @@ brisRadioChecked.addEventListener("click", brisRadio);
 
 //sends current location lat lon to weather API
 function handlePosition(position) {
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(getWeather);
   let timeZoneApiUrl = `https://secure.geonames.org/timezoneJSON?lat=${position.coords.latitude}&lng=${position.coords.longitude}&username=rack`;
@@ -186,7 +218,6 @@ function getCurrentPosition() {
 function getWeatherforCity(response) {
   let lat = response.data.coord.lat;
   let lon = response.data.coord.lon;
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(getWeather);
   let timeZoneApiUrl = `https://secure.geonames.org/timezoneJSON?lat=${lat}&lng=${lon}&username=rack`;
@@ -196,7 +227,6 @@ function getWeatherforCity(response) {
 function getSearchCity() {
   let input = document.getElementById("city-search").value;
   let cleanInput = input.toLowerCase().trim();
-  let apiKey = "721dfdcfc09e07da4b6904753634db8b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cleanInput}&appid=${apiKey}`;
   axios.get(apiUrl).then(getWeatherforCity);
 }
@@ -210,15 +240,6 @@ function enterPress(event) {
 
 //date & time function
 function currentDayTime(now) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   let day = days[now.getDay()];
 
   let hours = [
@@ -250,20 +271,20 @@ let localDayTime = document.querySelector("#daytime");
 localDayTime.innerHTML = currentDayTime(now);
 
 //convert temperature metrics
-function displayTempAsF(event) {
-  event.preventDefault();
-  let checkElement = farenheitLink.innerHTML;
-  let check = checkElement.includes("Farenheit");
+// function displayTempAsF(event) {
+//   event.preventDefault();
+//   let checkElement = farenheitLink.innerHTML;
+//   let check = checkElement.includes("Farenheit");
 
-  if (check == true) {
-    let farenheitTemp = Math.round((celsiusTemp * 9) / 5 + 32);
-    document.querySelector("#main-temp").innerHTML = `${farenheitTemp}°F`;
-    farenheitLink.innerHTML = "Convert to Celsius";
-  } else {
-    document.querySelector("#main-temp").innerHTML = `${celsiusTemp}°C`;
-    farenheitLink.innerHTML = "Convert to Farenheit";
-  }
-}
+//   if (check == true) {
+//     let farenheitTemp = Math.round((celsiusTemp * 9) / 5 + 32);
+//     document.querySelector("#main-temp").innerHTML = `${farenheitTemp}°F`;
+//     farenheitLink.innerHTML = "Convert to Celsius";
+//   } else {
+//     document.querySelector("#main-temp").innerHTML = `${celsiusTemp}°C`;
+//     farenheitLink.innerHTML = "Convert to Farenheit";
+//   }
+// }
 
 //triggers function to get current location on "Current" button click
 let currentButton = document.querySelector("#current-location");
@@ -274,6 +295,6 @@ searchButton.addEventListener("click", getSearchCity);
 //ensure search bar also works on enter keypress
 let searchInput = document.getElementById("city-search");
 searchInput.addEventListener("keypress", enterPress);
-//farenheit conversion
-let farenheitLink = document.querySelector("#farenheit-link");
-farenheitLink.addEventListener("click", displayTempAsF);
+// //farenheit conversion
+// let farenheitLink = document.querySelector("#farenheit-link");
+// farenheitLink.addEventListener("click", displayTempAsF);
